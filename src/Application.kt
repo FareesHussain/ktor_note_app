d94f5d629cc83015ef45ed53435c70ccc7597a89
farees hussain <fareezzhussain@gmail.com>
2020-09-03 19:22:53 +0530

    initail commit

---
+++ src/Application.kt
@@ -0,0 +1,14 @@
+package farees.hussain
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+}
+
